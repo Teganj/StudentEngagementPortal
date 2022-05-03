a3189f73f65e48fbbc8ae97b665ab6463cb0a864
teganj <teganjennings1@hotmail.com>
2022-05-03 23:25:32 +0100

    removing unused files

--- src/JavaScript/upload.js
+++
@@ -1,7 +0,0 @@
-const actualBtn = document.getElementById('actual-btn');
-
-const fileChosen = document.getElementById('fileToUpload');
-
-actualBtn.addEventListener('change', function(){
-    fileChosen.textContent = this.files[0].name
-})
--- src/PHP/importData.php
+++ src/PHP/importData.php
@@ -1,6 +1,16 @@
 <?php
 include_once 'connection.php';
 
+$statusMsg = '';
+
+// File upload path
+$targetDir = "../uploads/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+
+
 if(isset($_POST['importSubmit'])){
     //Allow types
     $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream',

6712cbceaa1fd2bcc33d6426d5e592d5c384c993
teganj <teganjennings1@hotmail.com>
2022-05-06 22:46:12 +0100

    Fixing user choice nav style

--- src/php/user_choice.php
+++ src/php/user_choice.php
@@ -8,7 +8,11 @@ $user_data = check_login($con);
 <html>
 <head>
     <title>Student Engagement Portal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 <?php include 'navbar.php' ?>

4303a126e9a8427a3482da5a545963946e2c57a2
teganj <teganjennings1@hotmail.com>
2022-05-06 23:25:43 +0100

    Finalised User Choice

--- src/php/user_choice.php
+++ src/php/user_choice.php
@@ -13,27 +13,40 @@ $user_data = check_login($con);
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <style>
+        #addModuleImg{
+            background-image: url("../img/viewModulesImg.jpg");
+            position: relative;
+            background-size: cover;
+        }
+        #viewModuleImg{
+            background-image: url("../img/addModuleImg.jpg");
+            position: relative;
+            background-size: cover;
+        }
+
+    </style>
 </head>
 <body>
 <?php include 'navbar.php' ?>
 
 <div class="container">
-    <!--    <h2>Welcome, --><?php //echo $user_data['name']; ?><!--! </h2>-->
-    <h2>Please choose an option below! </h2>
-    <div class="row">
+    <h1  style="text-align: center; font-weight: bold; margin: auto; padding-top: 50px;">Welcome, <?php echo $user_data['name']; ?> <br> Please choose an option below! </h1>
+    <div class="row" style="padding-top: 50px;">
 
-        <div class="card" style="width: 18rem; float: left">
+        <div class="card" id="addModuleImg" style="width: 40%; float: left; border: 5px solid #784794; padding: 10%; margin: auto;">
             <div class="w3-container w3-center w3-animate-left">
-                <img class="card-img-top w3-circle" src="../img/viewModulesImg.jpg" alt="View Modules">
+<!--                <img class="card-img-top" src="../img/viewModulesImg.jpg" alt="View Modules" style="width: 100%;">-->
                 <div class="card-body">
                     <a href="dashboard.php" class="btn btn-primary"><h5 class="card-title">View Module</h5></a>
                 </div>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem; float: right;">
+        <div class="card" id="viewModuleImg" style="width: 40%; float: right; border: 5px solid #784794; padding: 10%; margin: auto;">
             <div class="w3-container w3-center w3-animate-right">
-                <img class="card-img-top w3-circle" src="../img/addModuleImg.jpg" alt="Add a Module">
+<!--                <img class="card-img-top" src="../img/addModuleImg.jpg" alt="Add a Module"  style="width: 100%;">-->
                 <div class="card-body">
                     <a href="index.php" class="btn btn-primary"><h5 class="card-title">Add a Module</h5></a>
                 </div>

dab1f7d81c43194bd00185d9b3cb32301fac2c29
teganj <teganjennings1@hotmail.com>
2022-05-26 22:07:29 +0100

    Updating connection to cleardb heroku db

--- connection.php
+++ connection.php
@@ -1,8 +1,14 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "student_engagement_portal_db";
+//$dbhost = "localhost";
+//$dbuser = "root";
+//$dbpass = "";
+//$dbname = "student_engagement_portal_db";
+
+
+$dbhost = "eu-cdbr-west-02.cleardb.net";
+$dbuser = "b5180f50d8a282";
+$dbpass = "ab6a5a25";
+$dbname = "heroku_c20da7f7108e04f";
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
     die("failed to connect!");

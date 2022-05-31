40c7bdbdfa8b9b077fbb99d5a810d568be852e54
teganj <teganjennings1@hotmail.com>
2022-05-31 12:06:51 +0100

    Heroku

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con = mysqli_connect("localhost", "root", "", "heroku_c20da7f7108e04f");
-define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/php/heroku_c20da7f7108e04f/');
-define('SITE_PATH', 'http://eu-cdbr-west-02.cleardb.net/php/heroku_c20da7f7108e04f/');
+$con = mysqli_connect("localhost", "root", "", "student_engagement_portal_db");
+define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/php/student_engagement_portal_db/');
+define('SITE_PATH', 'http://127.0.0.1/php/student_engagement_portal_db/');
 define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
 ?>
--- connection.php
+++ connection.php
@@ -1,14 +1,8 @@
 <?php
-//$dbhost = "localhost";
-//$dbuser = "root";
-//$dbpass = "";
-//$dbname = "student_engagement_portal_db";
-
-
-$dbhost = "eu-cdbr-west-02.cleardb.net";
-$dbuser = "b5180f50d8a282";
-$dbpass = "ab6a5a25";
-$dbname = "heroku_c20da7f7108e04f";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "student_engagement_portal_db";
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
     die("failed to connect!");
